a01e617adb75b7b13fb41f366f4ac934fd2c5edd
Spartan-ash <ashishhsingh84@gmail.com>
2021-10-14 19:08:24 +0530

    Factorial - recursive approach

--- Recursion/Factorial.js
+++ Recursion/Factorial.js
@@ -10,3 +10,13 @@ function factorialOfANum(num) {
 
 console.log(factorialOfANum(3));
 console.log(factorialOfANum(5));
+
+//Using Recursion
+
+function factorialOfANumRecursiveWay(num) {
+  if (num === 1) return 1; //this is done to make a breakpoint else next statement will keep on multiplying in negative values
+  return num * factorialOfANumRecursiveWay(num - 1);
+}
+
+console.log(factorialOfANumRecursiveWay(2));
+console.log(factorialOfANumRecursiveWay(5));
