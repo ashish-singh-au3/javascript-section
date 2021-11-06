92897e906f89b4f15b801975bc2a6a4e965bb4e9
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-06 17:48:17 +0530

    Practice one - imp

---
+++ Practice-One/index.js
@@ -0,0 +1,10 @@
+function reverseAnInteger(n) {
+  let reversed = n.toString().split("").reverse().join("");
+  reversed = parseInt(reversed);
+
+  if (n < 0) {
+    return reversed * -1;
+  }
+  return reversed;
+}
+console.log(reverseAnInteger("-123"));
