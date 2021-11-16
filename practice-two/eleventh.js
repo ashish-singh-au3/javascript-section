f6539f08e97bc00835960e5c1984a3cfdc5052cf
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-16 13:58:21 +0530

    sec rev

---
+++ practice-two/eleventh.js
@@ -0,0 +1,12 @@
+function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase().replace(/[\W_]+/g, "");
+  str2 = str2.toLowerCase().replace(/[\W_]+/g, "");
+
+  const str1Sorted = str1.split("").sort().join("");
+  const str2Sorted = str2.split("").sort().join("");
+
+  return str1Sorted === str2Sorted;
+}
+
+console.log(isAnagram("!raT", "taR")); //t
+console.log(isAnagram("!raT", "taRet")); //f
