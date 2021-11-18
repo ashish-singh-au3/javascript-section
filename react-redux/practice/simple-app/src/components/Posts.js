d213d1e81d7948e03afe64adbe9b64c87f183c0f
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-18 12:38:37 +0530

    react red post

--- react-redux/practice/simple-app/src/components/Posts.js
+++ react-redux/practice/simple-app/src/components/Posts.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import { fetchPosts } from "../actions/postActions";
@@ -19,6 +19,10 @@ class Posts extends Component {
   }
 }
 
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+};
 const mapStateToProps = (state) => ({
   posts: state.posts.items,
 });
