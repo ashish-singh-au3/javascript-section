0554df3f63aa4082622af94d48bcddb790ade4be
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-20 15:50:50 +0530

    forEach

---
+++ advanced-arr-methods/forEach-One.js
@@ -0,0 +1,14 @@
+//console,log colors
+
+const colors = ["red", "green", "blue"];
+
+//first method
+colors.forEach(function (color) {
+  console.log(color);
+});
+
+//sec method
+
+colors.forEach((color) => {
+  console.log(color);
+});
