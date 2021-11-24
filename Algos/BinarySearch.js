4b85dd7dbb9325a7733eb92d0a45be6f4c99de5d
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-24 18:55:15 +0530

    Algos***

---
+++ Algos/BinarySearch.js
@@ -0,0 +1,24 @@
+// Implement a Binary Search Algorithm that searches a Sorted Array
+// for the provided value.
+// The Binary Search Algorithm will return the index of value found, or -1 if not found.
+
+const binarySearch = (sortedArray, value) => {
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midValue = sortedArray[mid];
+
+    if (midValue === value) {
+      return mid;
+    } else if (midValue < value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(binarySearch([5, 10, 15, 20, 25, 32, 40], 25)); //4
