1eee044fa948f2d99b71b15c31a3adf8c77f68e0
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-25 15:17:07 +0530

    react pr

--- react-redux/practice/todo/src/App.js
+++ react-redux/practice/todo/src/App.js
@@ -3,16 +3,26 @@ import React from "react";
 import { useState } from "react";
 
 import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 import "./App.css";
 
 function App() {
   const [inputText, setInputText] = useState("");
+
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="">
       <h1 className="heading">
         <center>Ashish's ToDo List</center>
       </h1>
-      <Form setInputText={setInputText} />
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList />
     </div>
   );
 }
