648dc514327a73a145388af763e07779f98fcee4
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-26 19:46:27 +0530

    Algos pr

---
+++ pr-3/19.js
@@ -0,0 +1,20 @@
+function stringPatternSearch(text, pattern) {
+  let countOfThePattern = 0;
+
+  for (let i = 0; i < text.length; i++) {
+    for (let j = 0; j < pattern.length; j++) {
+      const patternChar = pattern[j];
+
+      if (patternChar !== text[i + j]) {
+        break;
+      }
+      if (j === pattern.length - 1) {
+        countOfThePattern++;
+      }
+    }
+  }
+  return countOfThePattern;
+}
+
+console.log(stringPatternSearch("lollipop", "lol")); //1
+console.log(stringPatternSearch("lolol", "lol")); //2
