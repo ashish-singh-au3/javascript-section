794c6cba698e431cfe3bc4ef6cc75066477d0216
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-26 18:17:31 +0530

    react pr1

--- react-redux/another-rev/src/components/ToDo.js
+++ react-redux/another-rev/src/components/ToDo.js
@@ -4,18 +4,28 @@ import Button from "@mui/material/Button";
 import CheckIcon from "@mui/icons-material/Check";
 import CloseIcon from "@mui/icons-material/Close";
 
-const ToDo = ({ todos, setTodos, text }) => {
+const ToDo = ({ todos, setTodos, text, todo }) => {
+  const deleteHandler = () => {
+    setTodos(todos.filter((el) => el.id !== todo.id));
+  };
   return (
     <div>
       <center>
         {" "}
-        <li className="todo-items">{text}</li>
-        <Button style={{ margin: "10px" }} type="submit" variant="contained">
-          <CheckIcon />
-        </Button>
-        <Button style={{ margin: "10px" }} type="submit" variant="contained">
-          <CloseIcon />
-        </Button>
+        <li className="todo-items">
+          <h2>{text}</h2>{" "}
+          <Button style={{ margin: "10px" }} type="submit" variant="contained">
+            <CheckIcon />
+          </Button>
+          <Button
+            onClick={deleteHandler}
+            style={{ margin: "10px" }}
+            type="submit"
+            variant="contained"
+          >
+            <CloseIcon />
+          </Button>
+        </li>
       </center>
     </div>
   );
