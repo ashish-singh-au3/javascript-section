2033cc18a98f9e2237257565c79c42548192db65
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-27 18:31:27 +0530

    Algos pr

---
+++ pr-3/20.js
@@ -0,0 +1,21 @@
+const binarySearch = (sortedArray, valueToBeSearched) => {
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midValue = sortedArray[mid];
+
+    if (midValue === valueToBeSearched) {
+      return mid;
+    } else if (midValue < valueToBeSearched) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return midValue;
+};
+
+console.log(binarySearch([5, 10, 15, 20, 25, 32, 40], 25)); //4
