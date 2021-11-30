c566c9be38ecb5452b24261f5523349c48c26e8c
Spartan-ash <ashishhsingh84@gmail.com>
2021-11-30 17:04:29 +0530

    arr method

---
+++ arr-methods/filter.js
@@ -0,0 +1,13 @@
+const words = [
+  "spray",
+  "limit",
+  "elite",
+  "exuberant",
+  "destruction",
+  "present",
+  "ashish",
+];
+
+const newSetOfWords = words.filter((word) => word.length >= 6);
+
+console.log(newSetOfWords);
