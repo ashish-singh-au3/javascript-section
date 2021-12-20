bb3898094a5265f449d3aa1b2ec8ed273a742ddb
Spartan-ash <ashishhsingh84@gmail.com>
2021-12-20 16:30:49 +0530

    map

--- advanced-arr-methods/map.js
+++ advanced-arr-methods/map.js
@@ -1,10 +1,17 @@
 const nums = [1, 2, 3];
-const doubleNums = []; //we have to fill this with value 2,4,6
+//const doubleNums = []; //we have to fill this with value 2,4,6
 
 //standard way
 
-for (let i = 0; i < nums.length; i++) {
-  doubleNums.push(nums[i] * 2);
-}
+//for (let i = 0; i < nums.length; i++) {
+//doubleNums.push(nums[i] * 2);
+//}
 
+//console.log("doubleNums", doubleNums); // (3)[2,4,6]
+
+//using map
+
+const doubleNums = nums.map((num) => {
+  return num * 2;
+});
 console.log("doubleNums", doubleNums); // (3)[2,4,6]
