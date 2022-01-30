ea69bfcef890ffa36ebeae3b6ba4f0c7fe1ec6b0
Spartan-ash <ashishhsingh84@gmail.com>
2022-01-30 19:26:12 +0530

    pr3

---
+++ pr-3/87.js
@@ -0,0 +1,19 @@
+const personDetails = [
+  { name: "qwert", birth: 1990, city: "Nevada" },
+  { name: "chris", birth: 1997, city: "LA" },
+  { name: "bum", birth: 1970, city: "DC" },
+];
+
+console.log("person detail array:", personDetails);
+//add age
+
+const personDetailsArrayWithAge = personDetails.map((newPersonDetails) => {
+  return {
+    name: newPersonDetails.name,
+    birth: newPersonDetails.birth,
+    city: newPersonDetails.city,
+    age: new Date().getFullYear() - newPersonDetails.birth,
+  };
+});
+
+console.log("new array with age :", personDetailsArrayWithAge);
