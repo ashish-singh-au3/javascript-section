9f0e4a61df3b14db26aca3fdcfb07d121983c902
Spartan-ash <ashishhsingh84@gmail.com>
2022-02-16 15:11:04 +0530

    differences

---
+++ pr4/7.js
@@ -0,0 +1,16 @@
+//null vs undefined vs not defined
+
+//console.log(typeof null); // object because null can be given as a value
+//console.log(typeof undefined); // undefined
+
+//ex
+
+let a;
+
+console.log(a); //undefined
+
+let b = null;
+
+console.log(b); //null
+
+//console.log(c); //not defined because c is not defined
