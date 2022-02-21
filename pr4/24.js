61f57b722f9a202f4250f121d5a9c3a112f97d04
Spartan-ash <ashishhsingh84@gmail.com>
2022-02-21 17:15:42 +0530

    pr4

--- pr4/24.js
+++ pr4/24.js
@@ -10,3 +10,15 @@ console.log(hoistedFunction(2));
 function hoistedFunction(n) {
   return n * n * n;
 }
+
+//undefined
+
+let x;
+
+console.log(x); //unde
+
+let y = null;
+
+console.log(y); //null
+
+//console.log(z); //not defined
