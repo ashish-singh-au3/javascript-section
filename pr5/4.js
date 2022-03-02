96d77921594ab3bca94bc3f8c7da8a16b66a0057
Spartan-ash <ashishhsingh84@gmail.com>
2022-03-02 19:28:14 +0530

    pr 5

--- pr5/4.js
+++ pr5/4.js
@@ -1,10 +1,10 @@
-const callBack = () => {
-  let a = 3;
-  let b = 3;
-  console.log("This will print after 2 sec that is", a + b);
-};
+// const callBack = () => {
+//   let a = 3;
+//   let b = 3;
+//   console.log("This will print after 2 sec that is", a + b);
+// };
 
-setTimeout(callBack, 2000);
+// setTimeout(callBack, 2000);
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -17,3 +17,38 @@ let result = array
   });
 
 console.log(result);
+
+const details = [
+  { name: "kris", city: "chennai", born: 1998 },
+  { name: "Christopher", city: "Delhi", born: 1995 },
+
+  { name: "Courtney", city: "Kanpur", born: 1999 },
+
+  { name: "Tim", city: "Chandigarh", born: 1998 },
+];
+
+const printUsingMap = details
+  .map((detail) => {
+    return {
+      name: detail.name,
+      city: detail.city,
+      born: detail.born,
+      age: new Date().getFullYear() - detail.born,
+    };
+  })
+  .filter((detail) => {
+    return detail.age <= 24;
+  });
+
+console.log(printUsingMap);
+
+const useFind = details.find((detail) => {
+  return detail.born === 1998;
+});
+console.log("Find use", useFind);
+
+const useOfForEach = details.forEach((detail, i) => {
+  return detail[1];
+});
+
+console.log(useOfForEach);
