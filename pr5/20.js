0518078a44157d506ba7ca9da4b583db34ff8a84
Spartan-ash <ashishhsingh84@gmail.com>
2022-03-13 19:47:34 +0530

    pr5

---
+++ pr5/20.js
@@ -0,0 +1,19 @@
+function searchVal(arr, pattern) {
+  let countOfPattern = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < pattern.length; j++) {
+      const patternChar = pattern[j];
+
+      if (patternChar !== arr[i + j]) {
+        break;
+      }
+
+      if (j === pattern.length - 1) {
+        countOfPattern++;
+      }
+    }
+  }
+  return countOfPattern;
+}
+
+console.log(searchVal("lollipop", "lol"));
