d89a94c9e61ff81153df9f8e9ba554d16face0cf
Spartan-ash <ashishhsingh84@gmail.com>
2022-03-19 16:01:40 +0530

    pr6

--- pr-6/2.js
+++ pr-6/2.js
@@ -18,3 +18,21 @@ const binarySearch = (sortedArray, value) => {
 };
 
 console.log(binarySearch([1, 2, 3, 4, 5, 6], 6));
+
+function strPatternSearch(text, pattern) {
+  let countOfPattern = 0;
+  for (let i = 0; i < text.length; i++) {
+    for (let j = 0; j < pattern.length; j++) {
+      const patternChar = pattern[j];
+
+      if (patternChar !== text[i + j]) {
+        break;
+      }
+      if (j === pattern.length - 1) {
+        countOfPattern++;
+      }
+    }
+  }
+  return countOfPattern;
+}
+console.log(strPatternSearch("Lolollol", "lol"));
