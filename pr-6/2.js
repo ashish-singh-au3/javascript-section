e67d281ae0ab3891912578da1782ae8d3f24dfa8
Spartan-ash <ashishhsingh84@gmail.com>
2022-03-19 15:57:56 +0530

    pr6

---
+++ pr-6/2.js
@@ -0,0 +1,20 @@
+const binarySearch = (sortedArray, value) => {
+  let left = 0;
+  let right = sortedArray.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midVal = sortedArray[mid];
+
+    if (midVal === value) {
+      return mid;
+    } else if (midVal < value) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6));
