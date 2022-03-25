aec146308547a0ab16c9f8b77dbba4ed797f26d3
Spartan-ash <ashishhsingh84@gmail.com>
2022-03-25 18:40:29 +0530

    pr6

--- pr-6/6.js
+++ pr-6/6.js
@@ -1,3 +1,11 @@
+document.querySelector("#products").addEventListener("click", (e) => {
+  console.log(e);
+
+  if (e.target.tagName === "LI") {
+    window.location.href += "#" + e.target.id;
+  }
+});
+
 let count = 0;
 const fetchData = () => {
   console.log("Fetching the data...", count++);
